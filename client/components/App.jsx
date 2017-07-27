6ef65b412f973c53e8de881d202f77930b7be7b0
sherlyc <sherlyc@gmail.com>
2017-07-28 10:57:46 +1200

    home is working

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import {HashRouter as Router, Route} from 'react-router-dom'
 
 const App = () => {
   return (
-    <h1>React development has begun!</h1>
+    <h1>Unicorn car rental!</h1>
+         <Route exact path = "/" component={Home} />
   )
 }
 
 export default App
-
