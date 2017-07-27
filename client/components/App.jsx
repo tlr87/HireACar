3b3aa0d7755a0d93bf855c098b779e4e81e5e555
sherlyc <sherlyc@gmail.com>
2017-07-28 11:57:29 +1200

    vehicles details working

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,17 +1,23 @@
 import React from 'react'
 import {HashRouter as Router, Route} from 'react-router-dom'
 import Home from './Home'
+import Vehicles from './Vehicles'
+import VehicleTypes from './VehicleTypes'
+import VehicleDetails from './VehicleDetails'
 
 const App = () => {
   return (
     <div>
     <h1>Unicorn car rental!</h1>
-    <div>
-      <Router>
-         <Route exact path = "/" component={Home} />
-         </Router>
-         </div>
-       </div>
+        <Router>
+          <div>
+            <Route exact path = "/" component={Home} />
+            <Route path = "/" component={Vehicles} />
+            <Route path="/list/:vehicle" component={VehicleTypes} />
+            <Route path="/vehicle/:vehicle/:name" component={VehicleDetails} />
+          </div>
+        </Router>
+      </div>
   )
 }
 
